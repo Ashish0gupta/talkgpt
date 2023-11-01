795d9187769aec28b88393237b7a116a0549f179
Ashish Gupta <https://github.com/Ashish0gupta/talkgpt>
2023-11-01 11:41:27 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/", async(req,res)=>{
+    res.status(200).send({
+        message: "This is TalkGPT AI"
+    });
+});
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  });
+  app.post("/", async (req, res) => {
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: req.body.input,
+            temperature: 0,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        console.log("PASSED: ", req.body.input);
+
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (error) {
+        console.log("FAILED:", req.body.input);
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+app.listen(4000, ()=>console.log("Server is running on port 4000"));
